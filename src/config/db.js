213daece5b94ba581310aca55c1e1190fd064efe
Gusdevr <gustav.y@hotmail.com>
2025-04-13 19:58:25 -0300

    db-railway

--- src/config/db.js
+++ src/config/db.js
@@ -7,7 +7,12 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT, // <- importante
     dialect: "mysql",
+    dialectOptions: {
+      ssl: false, // pode ser necessário colocar isso como `true` em casos com SSL obrigatório
+    },
+    logging: false, // remove logs SQL se quiser
   }
 );
 
